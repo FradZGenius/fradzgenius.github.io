b62fc6a984639a5f28044360244493324778df35
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2019-12-18 11:32:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var db = firebase.database()
 
-db.ref('msgs/' + Math.random().toString(36).substring(7)).set({
+var usrname = prompt("Enter a username: ")
+
+db.ref('msgs/').on('value',function(snapshot){
+  msgDisplay = document.createElement("h1")
+  msgDisplay.innerHtml = snapshot.value().msg
   
 })
+
+function postMessage(message){
+  db.ref('msgs/' + Math.random().toString(36).substring(7) + usrname).set({
+		"msg":message,
+    "usr":usrname
+	})
+}
